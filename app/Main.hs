e6c7a0ede1e682348c20c0970393e0ffcf3ffe29
erik stevenson <eriknstevenson@gmail.com>
2016-06-08 07:53:30 -0400

    commandline argument parsing

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,37 @@
+{-# LANGUAGE DeriveGeneric     #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DataKinds         #-}
+{-# LANGUAGE TypeOperators     #-}
+
 module Main where
 
 import HardHat
 
+import Options.Generic
+import System.FilePath.Glob
+
+newtype Glob = Glob {pattern' :: Pattern} deriving (Show)
+
+instance ParseField Glob where
+  parseField h m = Glob . compile <$> parseField h m
+
+instance ParseRecord Glob where
+  parseRecord = fmap getOnly parseRecord
+
+instance ParseFields Glob where
+  parseFields = parseField
+
+instance ParseRecord Opts
+
+--example:
+data Opts = Opts
+  { inputGlob :: Maybe Glob <?> "Input file pattern"
+
+   --If no outputDir is supplied, send output to stdout.
+  , outputDir :: Maybe FilePath <?> "Directory to output generated files"
+  } deriving (Show, Generic)
+
 main :: IO ()
-main = return ()
+main = do
+  opts <- getRecord "test"
+  print (opts :: Opts)
